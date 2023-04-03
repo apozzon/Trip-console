e7ced4e68254b79c2af7fdff7e76ebfced0d2008
apozzon <andrea.pozzoni@gmail.com>
2023-04-03 20:00:50 +0200

    Initial Commit

---
+++ Trip console/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Trip console
+//
+//  Created by Andrea on 03/04/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
